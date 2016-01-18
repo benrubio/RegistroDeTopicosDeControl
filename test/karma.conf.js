4da513584c9764e72d491de44258078cc8ad95d7
benrubio <benrubio@outlook.com>
2016-01-18 14:36:14 -0800

    Adding jasmine-node tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function(config){
       'public/bower_components/angular-route/angular-route.js',
       'public/bower_components/angular-resource/angular-resource.js',
       'public/bower_components/angular-mocks/angular-mocks.js',
+      'public/javascripts/**/*.js',
       'test/unit/public/**/*.js'
     ],
 
