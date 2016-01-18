2579886830d3622877e9afbd4d0a9d56330da489
benrubio <benrubio@outlook.com>
2016-01-18 13:56:12 -0800

    Angular JS Test scripts and basic configurations

---
+++ test/unit/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+describe('Sample Test', function(){
+
+  it('should expect to pass', function() {
+
+    expect(1).toEqual(1);
+  });
+});
